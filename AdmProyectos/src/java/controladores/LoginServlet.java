5644ab012eca2b2d028b2ca5639c830cf843e2ac
aaguerra <alexis_17_91@hotmail.es>
2016-07-26 19:49:22 -0500

    cosas varias

--- AdmProyectos/src/java/controladores/LoginServlet.java
+++ AdmProyectos/src/java/controladores/LoginServlet.java
@@ -7,7 +7,6 @@ package controladores;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import emtity.Usuario;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.ejb.EJB;
@@ -16,7 +15,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import jpa.UsuarioFacade;
 
 /**
  *
@@ -24,9 +22,6 @@ import jpa.UsuarioFacade;
  */
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
-
-    @EJB
-    private UsuarioFacade jpa_user;
     
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -57,10 +52,6 @@ public class LoginServlet extends HttpServlet {
         out.print(gson.toJson(object));
         out.flush();
         
-        Usuario u1= jpa_user.find(new Usuario(""));
-        
-        Usuario u = new Usuario(usuario, password, usuario, usuario, usuario);
-        jpa_user.create(u);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
